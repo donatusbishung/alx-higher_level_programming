f2d28a18da87083dd076e78fef7fa8c1bbe166ba
donatusbishung <donaldbishung@yahoo.com>
2022-12-23 23:17:26 +0100

    tuple divided by two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is None:
-        return
-    new_list = []
-    for n in my_list:
-        if n % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-        return new_list
+    store = []
+    for item in my_list:
+        store.append(item % 2 == 0)
+        return (store)
