e530483ad9b7831013215ee549b297ccf1a2a7bf
donatusbishung <donaldbishung@yahoo.com>
2024-03-17 11:20:28 +0100

    print combinations of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
-for i in range(10):
+for i in range(9):
     for j in range(10):
-        if i != j:
-            print(f'{i}{j}', end=", ")
-    print()
+        if i < j:
+            if i != 8 or (i == 8 and j != 9):
+                # print(f'{i}{j}', end=" ")
+                print("{:d}{:d}".format(i, j), end=" ")
+                # print("{:02}".format(i*10 + j), end=" ")
+            else:
+                print("{:d}{:d}".format(i, j))
