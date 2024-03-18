2f35442ae279211f807e75fac57e2e14de61c1cd
donatusbishung <donaldbishung@yahoo.com>
2024-03-18 09:23:36 +0100

    function that converts lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for character in range(len(str)):
+        if ord(str[character]) >= 97 and ord(str[character]) <= 122:
+            number = 32
+        else:
+            number = 0
+        print("{:c}".format(ord(str[character])- number), end="")
+    print()
