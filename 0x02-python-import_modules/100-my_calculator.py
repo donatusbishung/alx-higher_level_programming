6cd9300133caaa5b487b43584b85fe951454940e
donatusbishung <donaldbishung@yahoo.com>
2024-03-21 05:11:16 +0100

    Arguments

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    new_args = len(argv) - 1
+    if new_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operation = argv[2]
+    if operation != '+' and operation != '-' and operation != '*' and operation != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if operation == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operation == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operation == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
